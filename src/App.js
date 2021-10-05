d6bd47196ef18b3d90b3b3b089afb8589d66d12e
Mohamed Hassanin Omran <mohamed_hassanin_omran@yahoo.com>
2021-10-05 19:10:40 +0200

    Lesson 17. Clear Users From State

--- src/App.js
+++ src/App.js
@@ -21,13 +21,21 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
   render() {
+    const { users, loading } = this.state;
     return (
       <div className="App">
         <Navbar title="GitHub Finder" icon="fab fa-github" />
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search
+            clearUsers={this.clearUsers}
+            searchUsers={this.searchUsers}
+            showClear={users.length > 0}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,6 +8,8 @@ export default class Search extends Component {
 
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
   };
 
   onChange = (e) => {
@@ -21,6 +23,8 @@ export default class Search extends Component {
   };
 
   render() {
+    const { showClear, clearUsers } = this.props;
+
     return (
       <div>
         <form className="form" onSubmit={this.onSubmit}>
@@ -37,6 +41,11 @@ export default class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
