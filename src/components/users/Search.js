469d8b81ceaee0a15478a3f8345ffb5ca84808f8
Mohamed Hassanin Omran <mohamed_hassanin_omran@yahoo.com>
2021-10-05 17:36:48 +0200

    Lesson 15. Events & Search Component

---
+++ src/components/users/Search.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+export default class Search extends Component {
+  state = {
+    text: "",
+  };
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  render() {
+    return (
+      <div>
+        <form className="form" onSubmit="onSubmit">
+          <input
+            type="text"
+            name="text"
+            placeholder="Search Users..."
+            value={this.state.text}
+            onChange={this.onChange}
+          />
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-dark btn-block"
+          />
+        </form>
+      </div>
+    );
+  }
+}
