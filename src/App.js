1e997fe818ed53b317b5823df364d4beb2e875ac
Mohamed Hassanin Omran <mohamed_hassanin_omran@yahoo.com>
2021-10-05 16:08:26 +0200

    Lesson 9. Getting Started With Component State
    - Add UserItem state to the class component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import './App.css';
 import NavBar from "./components/layout/NavBar"
+import UserItem from "./components/users/UserItem"
 
 class App extends Component {
 
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <NavBar title="GitHub Finder" icon="fab fa-github"/>
+        <UserItem />
       </div>
     );
   }
