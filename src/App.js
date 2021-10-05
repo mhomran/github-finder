49e39fdae8380a17a4749bbb772fbcee79223807
Mohamed Hassanin Omran <mohamed_hassanin_omran@yahoo.com>
2021-10-05 16:24:02 +0200

    Lesson 10. Lists & Passing State With Props
    - using map() to render multiple UserItem

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react'
 import './App.css';
 import NavBar from "./components/layout/NavBar"
-import UserItem from "./components/users/UserItem"
-
+import Users from "./components/users/Users"
+ 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
         <NavBar title="GitHub Finder" icon="fab fa-github"/>
-        <UserItem />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
