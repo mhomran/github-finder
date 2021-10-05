bd7c51daff9b47773a2436c2d5eb19ce10ba1cde
Mohamed Hassanin Omran <mohamed_hassanin_omran@yahoo.com>
2021-10-05 14:34:51 +0200

    Lesson 7: Expressions & Conditionals in JSX
    - ternary operation (expression ? A : B)
    - && operator ex: (showName && name)

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import './App.css';
 class App extends Component {
 
   render() {
+    const name = "Mo Salah";
+    const loading = false;
+    const showName = true;
+
     return (
       <div className="App">
-        <h1>Hello From React</h1>
+        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
       </div>
     );
   }
