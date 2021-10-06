8b8b3877fd70cf5ee54535f59700b79490c65232
Mohamed Hassanin Omran <mohamed_hassanin_omran@yahoo.com>
2021-10-06 20:35:23 +0200

    Lesson 22. Repos & RepoItem Component & Data

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -2,14 +2,19 @@ import React, { Fragment, Component } from "react";
 import { Spinner } from "../layout/Spinner";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { Repos } from "../repos/Repos";
+
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
   static propTypes = {
     loading: PropTypes.bool,
     user: PropTypes.object.isRequired,
     getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
+    repos: PropTypes.array.isRequired,
   };
   render() {
     const {
@@ -28,7 +33,7 @@ export class User extends Component {
       company,
     } = this.props.user;
 
-    const loading = this.props.loading;
+    const { loading, repos } = this.props;
 
     if (loading) return <Spinner />;
 
@@ -95,6 +100,7 @@ export class User extends Component {
           <div className="badge badge-light">Public Repos: {public_repos}</div>
           <div className="badge badge-dark">Public Gists: {public_gists}</div>
         </div>
+        <Repos repos={repos} />
       </Fragment>
     );
   }
